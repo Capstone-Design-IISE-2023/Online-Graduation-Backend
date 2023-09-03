dec5bdf7b89fb754f15dd050d6f576339ceb9674
eunbin00 <eun61n@gmail.com>
2023-09-03 20:15:46 +0900

    Fix: linting

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """ Tests for Django admin """
 
@@ -13,22 +14,21 @@ class AdminSiteTests(TestCase):
         """ Create use and client """
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@example.com',
-            password = 'p@ssw0rd_admin'
+            email='admin@example.com',
+            password='p@ssw0rd_admin'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@example.com',
-            password = 'p@ssword_user',
-            name = 'Test User',
+            email='user@example.com',
+            password='p@ssword_user',
+            name='Test User',
         )
 
     def test_users_list(self):
         """ Test users are listed on page """
         url = reverse('admin:core_user_changelist')
-        res = self.client.get(url) # makes http request
+        res = self.client.get(url)
 
-        # checks if response(page content) contains user we make
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
