43b8858e64bff72c64fefc50845d40f4996bff48
se0hyun <kimlisa00@seoultech.ac.kr>
2023-09-10 13:31:24 +0900

    app

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("user/", include("user.urls")),
+    # path("user/", include("user.urls")),
     # path("sign-up/", views.SignupView.as_view(), name="create"),
     # path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     # path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
